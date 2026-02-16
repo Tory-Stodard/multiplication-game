3cc130912f05404e990df4e9a529757e537f21f5
Tory Stodard <125704757+Tory-Stodard@users.noreply.github.com>
2026-02-16 10:30:35 -0700

    Created function to generate a random problem.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function playGame() {
-  const playBtn = document.querySelector('.play-btn');
-  const scoreUI = document.querySelector('.score');
-  const problem = document.querySelector('.problem');
-  const playerAnswer = document.querySelector('.player-answer');
-  const submitBtn = document.querySelector('.submit-btn');
+const playBtn = document.querySelector('.play-btn');
+const scoreUI = document.querySelector('.score');
+const problem = document.querySelector('.problem');
+const playerAnswer = document.querySelector('.player-answer');
+const submitBtn = document.querySelector('.submit-btn');
 
+function playGame() {
   playBtn.classList.add('hidden');
   scoreUI.classList.remove('hidden');
   problem.classList.remove('hidden');
@@ -15,5 +15,12 @@ function playGame() {
 }
 
 function generateProblem() {
-  console.log('1*1?');
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+
+  problem.textContent = num1 + ' * ' + num2;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 13);
 }
