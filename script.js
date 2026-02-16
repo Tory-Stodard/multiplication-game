554e701c769dbf37afb7b1defc8f19619b01ba6e
Tory Stodard <125704757+Tory-Stodard@users.noreply.github.com>
2026-02-16 10:21:13 -0700

    Created playGame function.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Script Linked!');
+function playGame() {
+  const playBtn = document.querySelector('.play-btn');
+  const scoreUI = document.querySelector('.score');
+  const problem = document.querySelector('.problem');
+  const playerAnswer = document.querySelector('.player-answer');
+  const submitBtn = document.querySelector('.submit-btn');
+
+  playBtn.classList.add('hidden');
+  scoreUI.classList.remove('hidden');
+  problem.classList.remove('hidden');
+  playerAnswer.classList.remove('hidden');
+  submitBtn.classList.remove('hidden');
+
+  generateProblem();
+}
+
+function generateProblem() {
+  console.log('1*1?');
+}
